b846fb53ac4de0dc27657f148c3f60a680e120c7
qiuyixi <heziliorg@163.com>
2022-04-06 19:28:21 +0800

    add

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -1,5 +1,6 @@
 #include "lept.h"
 #include <assert.h>
+#include <stdlib.h> 
 
 #define EXPECT(c, ch) do { assert(*c->json == (ch)); c->json++; } while(0)
 
@@ -18,10 +19,15 @@ static void lept_parse_whitespace(lept_context* c)
 }
 
 
-static int lept_parse_null(lept_context* c, lept_type* v)
+static int lept_parse_null(lept_context* c, lept_value* v)
 {
     EXPECT(c, 'n');
+    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
+        return LEPT_PARSE_INVALID_VALUE;
     
+    c->json += 3;
+    v->type = LEPT_NULL;
+    return LEPT_PARSE_OK;    
 }
 
 /* value = null / false / true */
@@ -30,9 +36,9 @@ static int lept_parse_value(lept_context* c, lept_type* v)
 {
     switch (*c->json)
     {
-    case 'n': return lept_parse_null(c, v);
-    default:
-        break;
+        case 'n': return lept_parse_null(c, v);
+        default:
+            break;
     }
 }
 
@@ -42,6 +48,12 @@ int lept_parse(lept_value* v, const char* json) {
 
     c.json = json;
     v->type = LEPT_NULL;
-    lept_parse_whitespece(&c);
+    lept_parse_whitespace(&c);
     return lept_parse_value(&c, v);
+}
+
+lept_type lept_get_type(const lept_value* v) {
+    assert(v != NULL);
+
+    return v->type;
 }
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -21,10 +21,33 @@ static int test_pass = 0;
 /* encapsulation API, abstruct formal macro for calling */
 #define EXPECT_EQ_INT(expect, actual)  EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
+
+static void test_parse_null() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+    printf("v->type = %d\n", v.type);
+    printf("LEPT_NULL = %d\n", lept_get_type(&v));
+}
+
+static void test_parse_expect_value() {
+    lept_value v;
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+    
+}
+
+void test_parse() {
+    test_parse_null();
+}
+
 int main()
 {
     /* EXPECT_EQ_BASE(0, 11, 20, "%d");*/
-    EXPECT_EQ_INT(22, 30);
+    test_parse();
     printf("test_pass = %d\n", test_pass);
     return 0;
 }
