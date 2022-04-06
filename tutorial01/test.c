81a40f2189f618876cbfc533eb81b9853d6a19df
hezl <hezili@ponytech.cn>
2022-04-06 17:47:45 +0800

    add two test

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lept.h"
+#include "leptjson.h"
 
 static int main_ret = 0;
 static int test_count = 0;
@@ -21,10 +21,38 @@ static int test_pass = 0;
 /* encapsulation API, abstruct formal macro for calling */
 #define EXPECT_EQ_INT(expect, actual)  EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
+static void test_parse_null(){
+    lept_value v;
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "oknul"));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_except_value() {
+    lept_value v;
+    v.type = LEPT_FALSE;
+
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, ""));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+
+    v.type = LEPT_FALSE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, " "));
+    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse() {
+    test_parse_null();
+    test_parse_except_value();
+}
+
 int main()
 {
-    /* EXPECT_EQ_BASE(0, 11, 20, "%d");*/
-    EXPECT_EQ_INT(22, 30);
+    test_parse();
+    printf("test_count= %d\n", test_count);
     printf("test_pass = %d\n", test_pass);
+    printf("pass percent = %3.2f%%\n", test_pass * 100.0 / test_count);
     return 0;
 }
